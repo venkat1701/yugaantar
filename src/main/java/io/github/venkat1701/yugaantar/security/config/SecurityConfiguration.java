9745ebe4aeded36759c49f0dec5a1859667bd921
venkat1701 <thejeastdev@gmail.com>
2024-11-17 06:11:19 +0530

    [YUGA] added-role-based-annotations

--- src/main/java/io/github/venkat1701/yugaantar/security/config/SecurityConfiguration.java
+++ src/main/java/io/github/venkat1701/yugaantar/security/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import io.github.venkat1701.yugaantar.security.jwt.JwtValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,6 +19,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
