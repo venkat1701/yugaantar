5ef8a11cb260f591e20ae0c1c48be410c9c38592
venkat1701 <thejeastdev@gmail.com>
2024-11-19 00:54:10 +0530

    [YUGA] successful-method-security

--- src/main/java/io/github/venkat1701/yugaantar/commons/security/GenericSecurityEvaluator.java
+++ src/main/java/io/github/venkat1701/yugaantar/commons/security/GenericSecurityEvaluator.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Component("genericSecurityEvaluator")
 public class GenericSecurityEvaluator {
@@ -22,12 +23,19 @@ public class GenericSecurityEvaluator {
         System.out.println("Allowed roles: " + Arrays.toString(allowedRoles));
         System.out.println("User roles: " + authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
-                .anyMatch(authority -> Arrays.stream(allowedRoles)
-                        .anyMatch(role -> authority.equals(role.replace("ROLE_", "")))));
+                .reduce(String::concat)
+                .get()
+                .replace("{authority=ROLE_", "")
+                .replace("}", "")
+                .equals(allowedRoles[0])
+        );
 
         return authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
-                .anyMatch(authority -> Arrays.stream(allowedRoles)
-                        .anyMatch(role -> authority.equals(role.replace("ROLE_", ""))));
+                .reduce(String::concat)
+                .get()
+                .replace("{authority=ROLE_", "")
+                .replace("}", "")
+                .equals(allowedRoles[0]);
     }
 }
